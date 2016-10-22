e5dead67fcfbd8e9ca1e5a79198bb8be4277d60a
Unknown <siddu_787@hotmail.com>
2016-10-22 20:15:09 +1300

    Fixed stats error when changing files

--- newGUI/src/rewardsNStats/Word.java
+++ newGUI/src/rewardsNStats/Word.java
@@ -5,12 +5,16 @@ import javafx.beans.property.SimpleStringProperty;
 
 public class Word {
 	
+	/*
+	 * This is a class that represents a word, it is there so that the JTable in the stats section can be used to its best potential.
+	 */
+	
 	private SimpleStringProperty word = new SimpleStringProperty();
 	private SimpleIntegerProperty correct = new SimpleIntegerProperty();
 	private SimpleIntegerProperty faulted= new SimpleIntegerProperty();
 	private SimpleIntegerProperty failed = new SimpleIntegerProperty();
 	
-	
+	//Constructor for the methods
 	public Word(String word, int correct, int faulted, int failed){
 		this.word.set(word);
 		this.correct.set(correct);
@@ -18,6 +22,7 @@ public class Word {
 		this.failed.set(failed);
 	}
 	
+	//Getter methods for the properties.
 	
 	public SimpleStringProperty wordProperty(){
 		return word;
